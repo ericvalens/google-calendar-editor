74c6b6440a3bc1f8558b4d0080048aee41213ae6
Eric Valenzuela <eric_e_valens@yahoo.com>
2020-11-28 11:41:37 -0800

    improve calendar list view

--- webapp/apps/client/src/app/routing.module.ts
+++ webapp/apps/client/src/app/routing.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CalendarsComponent } from './calendars/calendars.component';
+import { CalendarsDetailComponent } from './calendars/calendars-detail/calendars-detail.component';
 import { LogsComponent } from './logs/logs.component';
 import { SettingsComponent } from './settings/settings.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'calendar', component: CalendarsComponent },
+  { path: 'detail/:id', component: CalendarsDetailComponent },
   { path: 'logs', component: LogsComponent },
   { path: 'settings', component: SettingsComponent },
   { path: '**', redirectTo: '/calendar' },
