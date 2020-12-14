e26bb221ae830741e5913f605845245726d37848
Eric Valenzuela <eric_e_valens@yahoo.com>
2020-12-13 23:12:43 -0800

    initial login implementation

--- webapp/apps/client/src/app/routing.module.ts
+++ webapp/apps/client/src/app/routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { LogsComponent } from './logs/logs.component';
 import { SettingsComponent } from './settings/settings.component';
 
 const routes: Routes = [
+  { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
   { path: 'logs', component: LogsComponent },
   { path: 'settings', component: SettingsComponent },
